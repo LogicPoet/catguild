7cc736483e745c7ad6aec26a4a92362eee31fe7b
LogicPoet <liuzhi0621@gmail.com>
2020-09-08 01:21:20 +0800

    项目初始化

---
+++ src/main/java/cn/catguild/Application.java
@@ -0,0 +1,24 @@
+package cn.catguild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 主启动类
+ * </p>
+ *
+ * @author zhi.liu
+ * @version V1.0
+ * @package cn.catguild
+ * @description
+ * @date Created in 2020-09-08 00:35
+ * @copyright Copyright (c) 2020
+ * @modified zhi.liu
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/cn/catguild/ApplicationTest.java
@@ -0,0 +1,19 @@
+package cn.catguild;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void main() {
+        System.out.println("测试成功！！！");
+        System.out.println("测试成功！！！");
+        System.out.println("测试成功！！！");
+    }
+
+}
