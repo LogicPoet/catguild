3ad14615d558cd7e90487912593fed8b6e91f1c5
zhi.liu <tony@wanyeup.com>
2021-06-30 18:56:33 +0800

    重置

--- src/main/java/cn/catguild/domain/bo/PostBO.java
+++
@@ -1,8 +0,0 @@
-package cn.catguild.domain.bo;
-
-/**
- * @author LZ
- * @date 2020/9/9 16:43
- **/
-public class PostBO {
-}
--- src/main/java/cn/catguild/domain/vo/PostVO.java
+++
@@ -1,8 +0,0 @@
-package cn.catguild.domain.vo;
-
-/**
- * @author LZ
- * @date 2020/9/9 16:42
- **/
-public class PostVO {
-}
